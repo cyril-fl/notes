248ee01ba49ac3abf5eb6af336bb2bd195b0d0c1
cyril-fl <cyril-f@hotmail.com>
2024-04-18 00:08:03 +0200

    * project save ** No big update

---
+++ notes/assets/myColor.swift
@@ -0,0 +1,18 @@
+//
+//  myColor.swift
+//  notes
+//
+//  Created by Cyril Flambard on 17/04/2024.
+//
+
+import SwiftUI
+
+struct myColor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    myColor()
+}
---
+++ notes/chapter/sectionView.swift
@@ -0,0 +1,18 @@
+//
+//  sectionView.swift
+//  notes
+//
+//  Created by Cyril Flambard on 17/04/2024.
+//
+
+import SwiftUI
+
+struct sectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    sectionView()
+}
